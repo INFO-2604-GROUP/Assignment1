9271700305900cd82d2ca4e3fd04235b7b142e26
reyhanmaha <reyhan12maharaj@gmail.com>
2023-02-14 17:13:30 -0400

    prompted users for keys

--- ProgramB.py
+++ ProgramB.py
@@ -1,11 +1,18 @@
 from socket import *
 
+alphabet=['\n',' ','!','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','^','_',',','~','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+              'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+size=26+26+24
+
 serverPort = 8080
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
 
+print("How many keys do you wish to use to crack the encryption")
+RailKEY_MAX=input("From 1 to (unknown), how many rail cipher keys do you want to try? : ")
+CeasrKEY_MAX=input("From 1 to (unknown), how many ceasar cipher keys do you want to try? : ")
 while 1:
     connectionSocket, addr = serverSocket.accept()
     CipherText = connectionSocket.recv(1024)
